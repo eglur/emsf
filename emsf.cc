1009b623cbd07149aba59d829be52490f0129fa0
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2015-01-29 17:19:49 -0200

    Created function generate_stochastic_matrix

--- emsf.cc
+++ emsf.cc
@@ -2,17 +2,28 @@
 #include "eigen3/Eigen/Dense"
 
 using namespace std;
+using namespace Eigen;
 
-// MatrixXd generate_stochastic_matrix(nrows, ncols)
-// {
-//   A = NULL;
-//   return A;
-// }
+MatrixXd generate_stochastic_matrix(int nrows, int ncols)
+{
+  int i;
+  
+  MatrixXd A = MatrixXd::Random(nrows,ncols).cwiseAbs();
+  MatrixXd result = A.rowwise().sum();
+  for(i = 0; i < ncols; i++) {
+    A.col(i) = A.col(i).array() / result.array();
+  }
+  return A;
+}
 
 
 int main()
 {
-  MatrixXd m = MatrixXd::Random(3,3);
-  cout << "m=" << endl << m << endl;
+  int nrows = 3;
+  int ncols = 3;
+
+  MatrixXd m = generate_stochastic_matrix(nrows, ncols);
+
+  cout << "m =" << endl << m << endl;
   return 0;
 }
