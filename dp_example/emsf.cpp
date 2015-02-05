4b51f3c056a0ad4193179e068fcde0427649c2e8
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2015-02-04 22:38:52 -0200

    []

--- dp_example/emsf.cpp
+++ dp_example/emsf.cpp
@@ -204,7 +204,7 @@ Real em_sf(model &md, v_data &dt, const Natural n, const Natural m, const Natura
         row_vec d_row = D[a[t+1]].row(y[t+1]);
         Real bb = B.row(t+1).cwiseProduct(d_row).sum();
 
-        row_vec k_row = K[a(t)].col(y(t+1)).transpose();
+        row_vec k_row = K[a[t]].col(y[t+1]).transpose();
         B.row(t) = bb * pi(y(t+1), a(t+1)) * k_row;
         B.row(t) = B.row(t) / NF(t);
       }
