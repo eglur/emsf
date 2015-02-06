6e99f9cb5d11253bd8158cd70b4f8eddae7d3c4d
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2015-02-06 14:42:15 -0200

    Plota de h a k

--- dp_example/log/plot.R
+++ dp_example/log/plot.R
@@ -8,28 +8,28 @@ D <- NULL
 ## tmp <- apply(tmp, 2, mean)
 ## D <- cbind(D, tmp)
 
-tmp <- read.table("/home/rafaelbeirigo/emsf/dp_example/log/e_emsf_b.log")
+## tmp <- read.table("/home/rafaelbeirigo/emsf/dp_example/log/e_emsf_b.log")
+## tmp <- apply(tmp, 2, mean)
+## D <- cbind(D, tmp)
+
+## tmp <- read.table("/home/rafaelbeirigo/emsf/dp_example/log/e_emsf_c.log")
+## tmp <- apply(tmp, 2, mean)
+## D <- cbind(D, tmp)
+
+tmp <- read.table("/home/rafaelbeirigo/log/e_emsf_h.log")
 tmp <- apply(tmp, 2, mean)
 D <- cbind(D, tmp)
 
-tmp <- read.table("/home/rafaelbeirigo/emsf/dp_example/log/e_emsf_c.log")
+tmp <- read.table("/home/rafaelbeirigo/log/e_emsf_i.log")
 tmp <- apply(tmp, 2, mean)
 D <- cbind(D, tmp)
 
-tmp <- read.table("/home/rafaelbeirigo/emsf/dp_example/log/e_emsf_d.log")
+tmp <- read.table("/home/rafaelbeirigo/log/e_emsf_j.log")
 tmp <- apply(tmp, 2, mean)
 D <- cbind(D, tmp)
 
-tmp <- read.table("/home/rafaelbeirigo/emsf/dp_example/log/e_emsf_e.log")
+tmp <- read.table("/home/rafaelbeirigo/log/e_emsf_k.log")
 tmp <- apply(tmp, 2, mean)
 D <- cbind(D, tmp)
 
-tmp <- read.table("/home/rafaelbeirigo/emsf/dp_example/log/e_emsf_f.log")
-tmp <- apply(tmp, 2, mean)
-D <- cbind(D, tmp)
-
-tmp <- read.table("/home/rafaelbeirigo/emsf/dp_example/log/e_emsf_g.log")
-tmp <- apply(tmp, 2, mean)
-D <- cbind(D, tmp)
-
-matplot(D, t="o")
+matplot(D, t="o" )
