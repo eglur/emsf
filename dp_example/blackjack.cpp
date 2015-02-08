f7aa0a79343e3d3a34bccf356d637b81f142cb9b
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2015-02-08 19:17:24 -0200

    blackjack.cpp: nova versão

--- dp_example/blackjack.cpp
+++ dp_example/blackjack.cpp
@@ -1,95 +1,159 @@
-#include "util.h"
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <iterator>
+#include <string>
+#include <vector>
 #include <ctime>
+#include <cmath>
+#include "eigen3/Eigen/Dense"
+#include "util.h"
+#include "emsf.h"
 
-/*
-* Generate random number that represents a card (1-13)
-*/
-int generate_card()
+
+using namespace std;
+using namespace Eigen;
+using namespace util;
+using namespace emsf;
+
+
+Natural sample_from_dist(vec dist)
 {
-  return rand() % 13 + 1;
+  Real v = random_Real();
+  Real vv = 0.0;
+  Natural ind = 0;
+
+  while (1) {
+    vv += dist[ind];
+    if (vv > v || abs(vv - v) < 1e-5)
+      break;
+    else
+      ++ind;
+  }
+
+  if (ind > dist.size() - 1)
+    cout << "ERRO MAIOR!!!";
+  
+  if (ind < 0)
+    cout << "ERRO MENOR!!!";
+  
+  return ind;
 }
 
-/*
-* The AI will always have a card between 15-21. This will generate their card.
-*/
-int generate_ai_card()
+
+Natural card(mat &D)
 {
-  return rand() % 7 + 15;
+  Natural c = sample_from_dist(D) + 1;
+  if (c > 10) c = 10;
+
+  return c;
 }
 
 
-
-/*
-* Let's play the game!
-*/
-int main()
+Natural draw_card(Natural &xc, Natural &x_ace, mat &D)
 {
-  // This creates a random seed for rand() based on the time.
-  srand(time(NULL));
+  Natural c = card(D);
 
-  bool play;
-  int ai_card, my_card;
-  char x;
+  xc += c;
 
-  std::cout << "Welcome to a very simple BlackJack game to kill time.\n";
+  if (c == 1 && !x_ace) {
+    xc += 10;
+    x_ace = 1;
+  }
 
-  play = true;
+  if (xc > 21 && x_ace) {
+    xc -= 10;
+    x_ace = 0;
+  }
+}
 
-  // Get the AI's card.
-  ai_card = generate_ai_card();
 
-  // Get your first card.
-  my_card = generate_card();
+bool transition(Natural &pc, Natural &p_ace, Natural &dc, Natural &d_ace, Natural &r, Natural a, bool &end, mat &D)
+{
+  Natural stick = 0;
+  Natural hit = 1;
+  
+  if (a == hit) {
+    draw_card(pc, p_ace, D);
 
-  std::cout << "You have " << my_card << " - (h)it or (s)tay?: ";
-
-  // Ask for a hit or stay.
-  do
-    {
-      std::cin >> x;
-
-      // If they have hit...
-      if ( x == 'h' )
-        {
-          // Generate a card, and add it on.
-          my_card = my_card + generate_card();
-
-          // Have they bust?
-          if ( my_card > 21 )
-            {
-              std::cout << "You got bust (" << my_card << ").\n";
-              play = false;
-            }
-          else
-            {
-              std::cout << "You now have " << my_card << " - (h)it or (s)tay?: ";
-            }
-        }
-      // If they stayed...
-      else if ( x == 's' )
-        {
-
-          // ... See who is bigger.
-          if ( my_card > ai_card )
-            {
-              // You WIN!
-              std::cout << "You won! (" << my_card << " vs " << ai_card << ")\n";
-              play = false;
-            }
-          else
-            {
-              // You have FAILED.
-              std::cout << "You lost! (" << my_card << " vs " << ai_card << ")\n";
-              play = false;
-            }
-        }
-      else
-        {
-          std::cout << "Please either type \"h\" to hit or \"s\" to stay: ";
-        }
+    if (pc > 21) {
+      r = -1;
+      end = true;
     }
-  while ( play == true );
-  // Some credits and stuff.
-  std::cout << "Thank you for playing Dion Moult's C++ BlackJack game.\n";
+    else {
+      end = false;
+    }
+  }
+  else if (a == stick) {
+    while (dc < 17)                                             /* TODO: checar se é < ou <= */
+      draw_card(dc, d_ace, D);
+
+    if (dc > 21)
+      r = 1;
+    else {
+      Natural p_diff = 21 - pc;
+      Natural d_diff = 21 - dc;
+
+      if (p_diff < d_diff)
+        r = 1;
+      else if (p_diff > d_diff)
+        r = -1;
+      else
+        r = 0;
+    }
+
+    end = true;
+  }
+}
+
+
+Natural get_s(Natural pc, Natural p_ace, Natural dc)
+{
+  return p_ace * 100 + pc * 10 + dc;
+}
+
+
+inline Natural get_a(Natural s, mat &pi)
+{
+  //TODO
+
   return 0;
+}
+
+
+Natural episode(mat &pi, mat &D)
+{
+  Natural pc = 0;
+  Natural p_ace = 0;
+  Natural dc = 0;
+  Natural d_ace = 0;
+  Natural r;
+  
+  draw_card(pc, p_ace, D);
+  draw_card(pc, p_ace, D);
+
+  draw_card(dc, d_ace, D);
+
+  bool end = false;
+  while (!end) {
+    Natural s = get_s(pc, p_ace, dc);
+    Natural a = get_a(s, pi);
+    transition(pc, p_ace, dc, d_ace, a, r, end, D);
+  }
+
+  return r;
+}
+
+
+Real evaluation(Natural n_eval, mat &pi, mat &D)
+{
+  Real E;
+
+  Natural R = 0;
+  for (Natural i = 0; i < n_eval; ++i)
+    R += episode(pi, D);
+
+  E = (double) R / (double) n_eval;
+  
+  return E;
 }
