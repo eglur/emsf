cdd0095e3ffa17e3eae512aa66a81fcd0ad65434
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2015-02-08 19:44:43 -0200

    emsf.h: Passar funções normalize e generate_stochastic_matrix

--- dp_example/emsf.h
+++ dp_example/emsf.h
@@ -28,6 +28,36 @@ namespace emsf
 
   typedef std::vector<data> v_data;
   typedef std::vector<mat> v_mat;
+
+
+  inline void normalize(mat &A)
+  {
+    for (Natural i = 0; i < A.rows(); ++i) {
+      Real sum = A.row(i).sum();
+      if (sum > 0) for (Natural j = 0; j < A.cols(); ++j) A(i,j) /= sum;
+    }
+  }
+
+
+  stoch_mat generate_stochastic_matrix(const Natural nrows, const Natural ncols, const bool constant = false, const bool one = false)
+  {
+    stoch_mat M;
+    if (one) {
+      M = mat::Zero(nrows, ncols);
+      for (Natural i = 0; i < nrows; ++i) {
+        M(i, util::random_Natural(0, ncols-1)) = 1.0;
+      }
+    }
+    else {
+      if (constant)
+        M = mat::Constant(nrows,ncols,1);
+      else
+        M = mat::Random(nrows,ncols).cwiseAbs();
+      normalize(M);
+    }
+
+    return M;
+  }
 }
 
 
