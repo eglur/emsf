445249154da3b65553e60788cde954edcca34668
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2015-02-09 01:23:21 -0200

    Obrigar a enviar o parâmetro 'save' da função episode
    Porque não sei ainda como fazer os restantes (do tipo vector) terem um
    valor padrão (como NULL (esse não pode, o g++ reclama que NULL é
    inteiro)).
    
    Assim que eu descobrir, save e os vetores de salvamento terão
    valores-padrão.  Até lá, ninguém tem :P

--- dp_example/blackjack.cpp
+++ dp_example/blackjack.cpp
@@ -97,7 +97,7 @@ inline Natural get_a(Natural s, mat &pi)
 }
 
 
-Natural episode(mat &pi, vec &card_dist, const bool save = false, std::vector<Natural> &yv, std::vector<Natural> &av, std::vector<Natural> &rv)
+Natural episode(mat &pi, vec &card_dist, const bool save, std::vector<Natural> &yv, std::vector<Natural> &av, std::vector<Natural> &rv)
 {
   Natural pc = 0, p_ace = 0;
   Natural dc = 0, d_ace = 0;
