1b9841f820ca93e302af155ba16b012d9d12b72b
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2015-02-09 16:15:34 -0200

    Adequar funções de geração de dados: parâmetros

--- dp_example/blackjack.cpp
+++ dp_example/blackjack.cpp
@@ -159,22 +159,22 @@ Real evaluation(Natural n_eval, mat &pi, vec &card_dist)
 }
 
 
-data generate_data_bj(model &md, vec &card_dist, const bool save)
+data_bj generate_data_bj(model &md, vec &card_dist)
 {
-  data dt;
+  data_bj dt;
   episode(md.pi, card_dist, true, dt.y, dt.a, dt.r);
 
   return dt;
 }
 
 
-v_data generate_batch_data_bj(model &md, const Natural T, const Natural num_batches)
+v_data generate_batch_data_bj(model &md, const Natural num_batches, vec &card_dist)
 {
-  v_data v_dt;
+  v_data_bj v_dt;
   v_dt.resize(num_batches);
 
   for (Natural i = 0; i < num_batches; ++i)
-    v_dt[i] = generate_data(md, T);
+    v_dt[i] = generate_data_bj(md, T);
 
   return v_dt;
 }
