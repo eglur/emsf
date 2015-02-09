bb74afc419bd914f320387ce64178bbffc6c3084
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2015-02-08 22:00:22 -0200

    Modificar card_dist para vec

--- dp_example/blackjack.cpp
+++ dp_example/blackjack.cpp
@@ -17,7 +17,7 @@ using namespace util;
 using namespace emsf;
 
 
-Natural card(mat &card_dist)
+Natural card(vec &card_dist)
 {
   Natural c = sample_from_dist(card_dist) + 1;
   if (c > 10) c = 10;
@@ -26,25 +26,25 @@ Natural card(mat &card_dist)
 }
 
 
-Natural draw_card(Natural &xc, Natural &x_ace, mat &card_dist)
+void draw_card(Natural &xc, Natural &x_ace, vec &card_dist)
 {
   Natural c = card(card_dist);
 
   xc += c;
 
-  if (c == 1 && !x_ace) {
+  if (!x_ace && c == 1) {
     xc += 10;
     x_ace = 1;
   }
 
-  if (xc > 21 && x_ace) {
+  if (x_ace && xc > 21) {
     xc -= 10;
     x_ace = 0;
   }
 }
 
 
-bool transition(Natural &pc, Natural &p_ace, Natural &dc, Natural &d_ace, Natural &r, Natural a, bool &end, mat &card_dist)
+void transition(Natural &pc, Natural &p_ace, Natural &dc, Natural &d_ace, Natural &r, Natural a, bool &end, vec &card_dist)
 {
   Natural stick = 0;
   Natural hit = 1;
@@ -93,16 +93,18 @@ inline Natural get_a(Natural s, mat &pi)
 {
   //TODO
 
-  return 1;
+  return rand() < 0.5;
 }
 
 
-Natural episode(mat &pi, mat &card_dist)
+Natural episode(mat &pi, vec &card_dist)
 {
   Natural pc = 0;
   Natural p_ace = 0;
+
   Natural dc = 0;
   Natural d_ace = 0;
+
   Natural r;
   
   draw_card(pc, p_ace, card_dist);
@@ -114,14 +116,28 @@ Natural episode(mat &pi, mat &card_dist)
   while (!end) {
     Natural s = get_s(pc, p_ace, dc);
     Natural a = get_a(s, pi);
-    transition(pc, p_ace, dc, d_ace, a, r, end, card_dist);
+    transition(pc, p_ace, dc, d_ace, r, a, end, card_dist);
+
+    // cout <<
+    //   pc <<
+    //   " " <<
+    //   p_ace <<
+    //   " " <<
+    //   dc <<
+    //   " " <<
+    //   d_ace <<
+    //   " " <<
+    //   a <<
+    //   " " <<
+    //   r <<
+    //   endl;
   }
 
   return r;
 }
 
 
-Real evaluation(Natural n_eval, mat &pi, mat &card_dist)
+Real evaluation(Natural n_eval, mat &pi, vec &card_dist)
 {
   Real E;
 
@@ -140,10 +156,12 @@ int main()
   Natural n = 200;
   Natural na = 2;
 
-  stoch_mat card_dist = generate_stochastic_matrix(10, 13, true).transpose();
+  vec card_dist = generate_stochastic_matrix(1, 13, true).transpose();
   stoch_mat pi = generate_stochastic_matrix(n, na, true);
 
-  Real E = evaluation(1, pi, card_dist);
+  Real E = evaluation(1000, pi, card_dist);
+
+  std::cout << E << std::endl;
   
   return 0;
 }
