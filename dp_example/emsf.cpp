15ae43c75c7120fae6fa0af7870d01351521856b
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2015-02-09 16:22:21 -0200

    Adicionar em_sf_sk()
    Inicialmente uma cópia da em_sf

--- dp_example/emsf.cpp
+++ dp_example/emsf.cpp
@@ -174,6 +174,86 @@ namespace emsf {
   }
 
 
+  Real em_sf_sk(model &md, v_data &dt, const Natural n, const Natural m, const Natural na, const Natural T, v_stoch_mat D, v_stoch_mat K, const Real eps = 1e-20, const Natural max_it = 10)
+  {
+    v_stoch_mat P = md.P;
+    vec mu = md.mu;
+    stoch_mat pi = md.pi;
+
+    Real old_score = minf;
+    Real score = 0.0;
+    Natural it = 0;
+
+    while (abs(score - old_score) > eps || it < max_it) {
+      old_score = score;
+      score = 0.0;
+
+      v_mat D2 = generate_zero_matrices(n, m, na);
+      v_mat K2 = generate_zero_matrices(m, n, na);
+
+      for (Natural batch = 0; batch < dt.size(); ++batch) {
+        vecn y = dt[batch].y;
+        vecn a = dt[batch].a;
+      
+        mat A = mat::Zero(T-1, m);
+        mat B = mat::Zero(T-1, m);
+        vec NF = vec::Zero(T-1);
+
+        A.row(0) = mu[y[0]] * pi(y[0], a[0]) * D[a[0]].row(y[0]);
+        NF[0] = A.row(0).sum();
+        A.row(0) = A.row(0) / NF[0];
+
+        for (Natural t = 1; t < T-1; ++t) {
+          row_vec k_row = K[a[t-1]].col(y[t]).transpose();
+          Real aa = A.row(t-1).cwiseProduct(k_row).sum();
+
+          A.row(t) = aa * pi(y[t], a[t]) * D[a[t]].row(y[t]);
+          NF[t] = A.row(t).sum();
+          A.row(t) = A.row(t) / NF[t];
+        }
+
+        Natural B_T = T-2;                                        // Different index strategy on the R language
+        B.row(B_T) = K[a[B_T]].col(y[B_T]).transpose();
+        B.row(B_T) = B.row(B_T) / NF[B_T];
+        for (Natural t = B_T-1; t >= 0; --t) {
+          row_vec d_row = D[a[t+1]].row(y[t+1]);
+          Real bb = B.row(t+1).cwiseProduct(d_row).sum();
+
+          row_vec k_row = K[a[t]].col(y[t+1]).transpose();
+          B.row(t) = bb * pi(y[t+1], a[t+1]) * k_row;
+          B.row(t) = B.row(t) / NF[t];
+        }
+
+        mat C = A.cwiseProduct(B);
+        normalize(C);
+
+        for (Natural t = 0; t < T-1; ++t) {
+          D2[a[t]].row(y[t]) = D2[a[t]].row(y[t]).array() + C.row(t).array();
+          K2[a[t]].col(y[t+1]) = K2[a[t]].col(y[t+1]).array() + C.row(t).transpose().array();
+        }
+
+        score = score - NF.array().inverse().log().sum();
+      }
+
+      for (Natural i = 0; i < na; ++i) {
+        normalize(D2[i]);
+        normalize(K2[i]);
+      }
+
+      D = D2;
+      K = K2;
+
+      ++it;
+    }
+
+    v_stoch_mat P_bar(na);
+    for (Natural i = 0; i < na; ++i)
+      P_bar[i] = D[i] * K[i];
+
+    return frobenius_norm_v(P_bar, P, na);
+  }
+
+
   Real em_sf(model &md, v_data &dt, const Natural n, const Natural m, const Natural na, const Natural T, v_stoch_mat D, v_stoch_mat K, const Real eps = 1e-20, const Natural max_it = 10)
   {
     v_stoch_mat P = md.P;
