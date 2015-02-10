1c711363308edaac59d1cc72d04fa8ed5148dac1
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2015-02-10 18:17:29 -0200

    Modificar parâmetro de data para bj

--- dp_example/emsf.cpp
+++ dp_example/emsf.cpp
@@ -174,7 +174,7 @@ namespace emsf {
   }
 
 
-  void em_sf_sk(model &md, v_data &dt, const Natural n, const Natural m, const Natural na, const Natural num_batches, v_stoch_mat D, v_stoch_mat K, const Real eps = 1e-20, const Natural max_it = 10)
+  void em_sf_sk(model &md, v_data_bj &dt, const Natural n, const Natural m, const Natural na, const Natural num_batches, v_stoch_mat D, v_stoch_mat K, const Real eps = 1e-20, const Natural max_it = 10)
   {
     vec mu = md.mu;
     stoch_mat pi = md.pi;
