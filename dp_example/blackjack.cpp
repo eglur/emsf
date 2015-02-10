1c49f9d4f26b689742f0e1b59a096a7891609e03
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2015-02-09 22:58:15 -0200

    TEST: printa a política aprendida pelo agente

--- dp_example/blackjack.cpp
+++ dp_example/blackjack.cpp
@@ -299,7 +299,7 @@ int main(int argc, char* argv[])
   pt_agent agt = policy_iteration(M, gamma);
 
   // Print value function
-  cout << *agt->V() << endl;
+  cout << *agt->pi() << endl;
 
   return 0;
 }
