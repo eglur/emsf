a5e49ed1fcff8f7fdf8da6339d80a758072363e3
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2015-02-11 19:09:40 -0200

    Preparar run.sh para levar em conta a qtd de cores da máquina

--- dp_example/run.sh
+++ dp_example/run.sh
@@ -7,9 +7,37 @@ die () {
 
 [ "$#" -eq 2 ] || die "Usage: run.sh START END"
 
-for i in $(eval echo {$1..$2})
+CORES=15
+START=$1
+END=$2
+
+LOCAL_START=$START
+LOCAL_END=`expr $START + $CORES - 1`
+
+if [ $LOCAL_END -gt $END ]
+then
+    LOCAL_END=$END
+fi
+
+while [ $LOCAL_START -le $END ]
 do
-    COMMAND="./blackjack $i 30000 1000000 0 100"
-    echo $COMMAND
-    $COMMAND &
+    while [ "$(pidof blackjack)" ]
+    do
+	sleep 1
+    done
+
+    for i in $(eval echo {$LOCAL_START..$LOCAL_END})
+    do
+	COMMAND="./blackjack $i 30000 1000000 0 100"
+	echo $COMMAND
+	$COMMAND &
+    done
+
+    LOCAL_START=`expr $LOCAL_END + 1`
+    LOCAL_END=`expr $LOCAL_START + $CORES - 1`
+
+    if [ $LOCAL_END -gt $END ]
+    then
+	LOCAL_END=$END
+    fi
 done
