e9e8eb883f0af5698b44de608dbb0511f6f76999
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2015-02-11 00:05:41 -0200

    FIX: add val NUM_POINTS

--- dp_example/run.sh
+++ dp_example/run.sh
@@ -9,7 +9,7 @@ die () {
 
 for i in $(eval echo {$1..$2})
 do
-    COMMAND="./blackjack $i 30000 1000000 0 30 1.0 300 75"
+    COMMAND="./blackjack $i 30000 1000000 0 100 30 1.0 300 75"
     echo $COMMAND
     $COMMAND &
 done
