dc2b4737f489f22aaae7f0c132262a6ee32d6c9c
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2015-02-11 21:46:04 -0200

    Preparar para limitar execução pelo número de cores da máquina

--- dp_example/run.sh
+++ dp_example/run.sh
@@ -7,17 +7,45 @@ die () {
 
 [ "$#" -eq 2 ] || die "Usage: run.sh START END"
 
-for M in 10 50 100
+CORES=15
+START=$1
+END=$2
+
+LOCAL_START=$START
+LOCAL_END=`expr $START + $CORES - 1`
+
+if [ $LOCAL_END -gt $END ]
+then
+    LOCAL_END=$END
+fi
+
+while [ $LOCAL_START -le $END ]
 do
     while [ "$(pidof blackjack)" ]
     do
 	sleep 1
     done
 
-    for i in $(eval echo {$1..$2})
+    for M in 10 50 100
     do
-	COMMAND="./blackjack $i 30000 1000000 0 100 30 1.0 300 $M"
-	echo $COMMAND
-	$COMMAND &
+	while [ "$(pidof blackjack)" ]
+	do
+	    sleep 1
+	done
+
+	for i in $(eval echo {$LOCAL_START..$LOCAL_END})
+	do
+	    COMMAND="./blackjack $i 30000 1000000 0 100 30 1.0 300 $M"
+	    echo $COMMAND
+	    $COMMAND &
+	done
     done
+
+    LOCAL_START=`expr $LOCAL_END + 1`
+    LOCAL_END=`expr $LOCAL_START + $CORES - 1`
+
+    if [ $LOCAL_END -gt $END ]
+    then
+	LOCAL_END=$END
+    fi
 done
