05914a7b424b90872b83c36f6ec503dc1f30e417
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2015-02-11 17:49:08 -0200

    Add all logs (they will be deleted)

--- dp_example/run.sh
+++ dp_example/run.sh
@@ -7,13 +7,17 @@ die () {
 
 [ "$#" -eq 2 ] || die "Usage: run.sh START END"
 
-for M in 10 20 30 40 50 60 70 80 90 100
+for M in 5 15
 do
+    while [ "$(pidof blackjack)" ]
+    do
+	sleep 1
+    done
+
     for i in $(eval echo {$1..$2})
     do
 	COMMAND="./blackjack $i 30000 1000000 0 100 30 1.0 300 $M"
 	echo $COMMAND
 	$COMMAND &
     done
-    sleep 1h
 done
