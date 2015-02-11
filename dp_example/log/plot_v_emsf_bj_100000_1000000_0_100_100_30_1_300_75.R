7958baa73e40ae9bc5c281bf39644fd6ce4a1cee
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2015-02-11 17:49:29 -0200

    Add all R files

---
+++ dp_example/log/plot_v_emsf_bj_100000_1000000_0_100_100_30_1_300_75.R
@@ -0,0 +1,5 @@
+D <- NULL
+
+matplot(D, t="l", main="EMSF-SK + PISF utilizando 100000 lotes (avaliando em 1000000 jogos)")
+grid()
+
---
+++ dp_example/log/plot_v_emsf_bj_30000_1000000_0_100_100_30_1_300_90.R
@@ -0,0 +1,9 @@
+D <- NULL
+
+tmp <- read.table("v_emsf_bj_30000_1000000_0_100_100_30_1_300_90.log")
+tmp <- apply(tmp, 2, mean)
+D <- cbind(D, tmp)
+
+matplot(D, t="l", main="v_emsf_bj_30000_1000000_0_100_100_30_1_300_90")
+grid()
+
