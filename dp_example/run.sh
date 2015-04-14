6c19100bc9c047d1955ac293ecdc6a5c50a8a516
Rafael Lemes Beirigo <rafaelbeirigo@gmail.com>
2015-04-14 11:44:22 -0300

    Documentar parâmetros do run.sh

--- dp_example/run.sh
+++ dp_example/run.sh
@@ -7,10 +7,27 @@ die () {
 
 [ "$#" -eq 2 ] || die "Usage: run.sh START END"
 
-CORES=14
-START=$1
-END=$2
+#############################
+# Parâmetros do experimento #
+#############################
+NUM_EPISODES=1000000    # Quantidade de episódios
 
+NUM_BATCHES=30000       # Quantidade final de batches utilizados
+MIN_BATCHES=0           # Quantidade inicial de batches utilizados
+NUM_POINTS=100          # Número total de pontos; o programa obtém internamente o incremento
+
+MAX_IT=30               # Número máximo de iterações do EM-SF
+
+GAMMA_PISF=1            # Valor do gamma utilizado no PISF
+MAX_IT_PISF=300         # Número máximo de iterações do PISF
+
+START=$1                # Número do experimento inicial (utilizado para diferenciar os nomes
+END=$2                  # Número do experimento final (utilizado para diferenciar os nomes dos arquivos de logs)
+
+CORES=10                # Quantidade de CPU cores da máquina que serão utilizados
+
+# LOCAL_START E LOCAL_END são utilizados para limitar a utilização de
+# cores de acordo com os parâmetros setados.
 LOCAL_START=$START
 LOCAL_END=`expr $START + $CORES - 1`
 
@@ -35,7 +52,7 @@ do
 
 	for i in $(eval echo {$LOCAL_START..$LOCAL_END})
 	do
-	    COMMAND="./blackjack $i 30000 1000000 0 100 30 1.0 300 $M"
+	    COMMAND="./blackjack $i $NUM_BATCHES $NUM_EPISODES $MIN_BATCHES $NUM_POINTS $MAX_IT $GAMMA_PISF $MAX_IT_PISF $M"
 	    echo $COMMAND
 	    $COMMAND &
 	done
