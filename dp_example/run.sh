f6823069a48787e2ac439d3b4ff4d0596b09c35c
Rafael Beirigo <rafaelbeirigo@gmail.com>
2015-09-13 14:23:50 -0300

    Exp: melhor intervalo de epsilon

--- dp_example/run.sh
+++ dp_example/run.sh
@@ -28,7 +28,7 @@ do
 
     for i in $(eval echo {$LOCAL_START..$LOCAL_END})
     do
-        for EPSILON in "0.01" "0.05" "0.10" "0.20" "0.3" "1.0"
+        for EPSILON in "0.01" "0.05" "0.10" "0.20" "0.30" "0.40" "0.50" "0.60" "0.70" "0.80" "0.90" "1.0"
         do
 	    COMMAND="./blackjack $i 30000 1000000 0 100 $EPSILON"
 	    echo $COMMAND
