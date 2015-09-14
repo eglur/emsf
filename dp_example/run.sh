97adcfeb0a62330ab14071089fc602ddf201f307
Rafael Beirigo <rafaelbeirigo@gmail.com>
2015-09-14 02:15:07 -0300

    run.sh: preparar para rodar primeiro experimento

--- dp_example/run.sh
+++ dp_example/run.sh
@@ -7,34 +7,48 @@ die () {
 
 [ "$#" -eq 2 ] || die "Usage: run.sh START END"
 
-NCORES=15
+NUM_BATCHES=5000
+NUM_EPISODES=1000000
+MIN_BATCHES=0
+NUM_POINTS=100
+MAX_IT=$NUM_BATCHES
+GAMMA_PISF=1
+MAX_IT_PISF=300
+
+NCORES=3
 START=$1
 END=$2
 
 PIDS=()
 for RUN in $(seq $START $END)
 do
-    for EPSILON in "0.01" "0.05" "0.10" "0.20" "0.30" "0.40" "0.50" "0.60" "0.70" "0.80" "0.90" "1.0"
+    for M in 10 100
     do
-        COMMAND="./blackjack $RUN 30000 1000000 0 100 $EPSILON"
-        echo $COMMAND
-        $COMMAND &
+        for ALPHA in "0.1" "0.4" "1.0"
+        do
+            for EPSILON in "0.1" "0.4" "1.0"
+            do
+                COMMAND="./blackjack $RUN $NUM_BATCHES $NUM_EPISODES $MIN_BATCHES $NUM_POINTS $MAX_IT $GAMMA_PISF $MAX_IT_PISF $M $EPSILON $ALPHA"
+                echo $COMMAND
+                $COMMAND &
 
-        # Adiciona pid do emsf que acabou de ser executado à lista de emsfs em execução
-        PIDS+=($!)
+                # Adiciona pid do emsf que acabou de ser executado à lista de emsfs em execução
+                PIDS+=($!)
 
-        # Somente continua se o tamanho da lista de pids em execução ainda não chegou ao limite (i.e., quantidade de cores)
-        # Enquanto isso, checa se os pids da lista ainda estão em execução e a atualiza
-        while [ ${#PIDS[@]} == $NCORES ]; do
-            sleep 1
-            # Para cada pid da lista
-            for i in $(seq 0 $((${#PIDS[@]} - 1))); do
-                if ! $(ps -p ${PIDS[$i]} &> /dev/null); then
-                    # Remove pid da lista se processo já terminou sua execução
-                    PIDS=(${PIDS[@]:0:$i} ${PIDS[@]:(($i +1))})
-                    # Sair, pois não é sensato iterar por índice em uma lista que acabou de ser alterada
-                    break
-                fi
+                # Somente continua se o tamanho da lista de pids em execução ainda não chegou ao limite (i.e., quantidade de cores)
+                # Enquanto isso, checa se os pids da lista ainda estão em execução e a atualiza
+                while [ ${#PIDS[@]} == $NCORES ]; do
+                    sleep 1
+                    # Para cada pid da lista
+                    for i in $(seq 0 $((${#PIDS[@]} - 1))); do
+                        if ! $(ps -p ${PIDS[$i]} &> /dev/null); then
+                            # Remove pid da lista se processo já terminou sua execução
+                            PIDS=(${PIDS[@]:0:$i} ${PIDS[@]:(($i +1))})
+                            # Sair, pois não é sensato iterar por índice em uma lista que acabou de ser alterada
+                            break
+                        fi
+                    done
+                done
             done
         done
     done
