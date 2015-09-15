97573dba58a01d0006fbacff999d2ee8370067d4
Rafael Beirigo <rafaelbeirigo@gmail.com>
2015-09-15 19:47:28 -0300

    Criei uma pasta para colocar os resultados de agora

--- dp_example/R/emsf.experiments.R
+++ dp_example/R/emsf.experiments.R
@@ -8,18 +8,18 @@ emsf.experiment.generate.P <- function(n, alpha.dir, na = 1)
 {
     P <- array(0, c(n, n, na))
     for (a in 1:na) P[,,a] <- rdirichlet(n,rep(alpha.dir, n))
-    P 
+    P
 }
 
 emsf.experiment.fixed.tc <- function(n, alpha.dir, m, tc, alphas, max.it, na = 1, num.avg = 50, dir = "./files/", idx = "")
 {
     R <- NULL
     D <- NULL
-    
+
     for (i in 1:length(alphas))
     {
         L <- NULL
-        
+
         for (j in 1:num.avg)
         {
             print(paste("Running alpha = ", alphas[i], "  run", j))
@@ -27,18 +27,18 @@ emsf.experiment.fixed.tc <- function(n, alpha.dir, m, tc, alphas, max.it, na = 1
             L <- cbind(L, emsf(P, m, tc, tc, alphas[i], max.it))
                                         #       print(L)
         }
-        
+
         R <- cbind(R, apply(L, 1, mean))
         colnames(R)[ncol(R)] <- paste(alphas[i])
-        
+
         D <- cbind(D, apply(L, 1, sd) / sqrt(num.avg))
         colnames(D)[ncol(D)] <- paste(alphas[i])
-        
+
         prefix <- ps(dir, paste("emsf", n, m, tc, max.it, alpha.dir, sep="_"))
         write.table(R, ps(prefix, idx, "_kld.txt"), row.names=FALSE, col.names=TRUE, quote=FALSE)
         write.table(D, ps(prefix, idx, "_ser.txt"), row.names=FALSE, col.names=TRUE, quote=FALSE)
     }
-    
+
     list(R = R, D = D)
 }
 
@@ -49,7 +49,7 @@ make.leg.tcs.alphas <- function(tcs, alphas) {
     for (tc in tcs)
     {
         for(alpha in alphas) {
-            l <- c(l, substitute(expression(t[c] == TC~alpha == ALPHA), list(TC=tc, ALPHA=alpha))[[2]]) 
+            l <- c(l, substitute(expression(t[c] == TC~alpha == ALPHA), list(TC=tc, ALPHA=alpha))[[2]])
         }
     }
                                         #    print(l)
@@ -72,19 +72,15 @@ emsf.experiment.plot.results.aaai <- function(save=FALSE,
                                               cex=NULL)
 {
     tcs <- c(600, 900)
-    pch1 <- c(5, 2, 1, 0)
-    pch2 <- c(18, 17, 16, 15)
+    pch1 <- c(5, 2, 1, 0, 6)
+    pch2 <- c(18, 17, 16, 15, 25)
 
-    pch1 <- c(5, 2, 1)
-    pch2 <- c(18, 17, 16)
-
-    col1 <- c("red", "green", "blue", "black")
-    col1 <- c("blue", "green", "red")
+    col1 <- c("orange", "green", "blue", "black", "red")
 
     emsf.experiment.plot.results(save=TRUE,
                                  tcs=tcs,
-                                 alphas=c(0.1, 0.3, 0.5, 0.7),
-                                 inds=c(1, 3, 4),
+                                 alphas=c(0.1, 0.3, 0.5, 0.7, 1.0),
+                                 inds=1:5,
                                  pch=c(pch1, pch2),
                                  col=c(col1, col1),
                                  ylim=ylim,
@@ -116,15 +112,15 @@ emsf.experiment.plot.results <- function(n = 100, m = 10,
         print(path)
         TMP <- as.matrix(read.table(path, header = TRUE))
         R <- cbind(R, TMP[seq(1, nrow(TMP), length=num.points),inds])
-        
+
         prefix <- ps(dir, paste("emsf", n, m, tcs[i], max.it, alpha.dir, sep="_"))
         TMP <- as.matrix(read.table(ps(prefix, idx, "_ser.txt"), header = TRUE))
         D <- cbind(D, TMP[seq(1, nrow(TMP), length=num.points),inds])
     }
-    
+
     ## print(R)
     mp(seq(1, max.it, l = num.points), R, R + D , R - D, xlab = expression(tau), ylab = expression("KL"[rho]*"(P, DK)", main=paste(tcs)), ylim=ylim, pch=pch, col=col, cex=cex)
-    
+
     l <- make.leg.tcs.alphas(tcs,alphas[inds])
     leg(pos=pos,l, pch=pch, col=col, cex=cex)
 
